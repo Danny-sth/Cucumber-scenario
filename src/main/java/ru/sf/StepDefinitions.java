87a630aec415abb514802f2f8f426db2dd62f6ef
Denis <danny.wasth@gmail.com>
2021-10-26 15:01:24 +0300

    Автотест для сайта H&M

--- src/main/java/ru/sf/StepDefinitions.java
+++ src/main/java/ru/sf/StepDefinitions.java
@@ -3,26 +3,61 @@ package ru.sf;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.junit.Assert.assertEquals;
+import java.time.Duration;
 
 public class StepDefinitions {
 
-    private String today;
-    private String actualAnswer;
+    private static final WebDriver WEB_DRIVER;
 
-    @Given("today is {string}")
-    public void today_is(String today) {
-        this.today = today;
+    static {
+        System.setProperty("webdriver.chrome.driver",
+                "/home/danny/Projects/cucumber-scenario/src/main/resources/chromedriver");
+        WEB_DRIVER = new ChromeDriver();
     }
 
-    @When("I ask about  whether it's Friday yet")
-    public void i_ask_about_whether_it_s_friday_yet() {
-        this.actualAnswer = WeekAnalizer.isItFridayToday(today);
+    @Given("url of H&M {string}")
+    public void url_of_h_m(String url) {
+        WEB_DRIVER.get(url);
     }
 
-    @Then("I should be told {string}")
-    public void i_should_be_told(String string) {
-        assertEquals("Nope", actualAnswer);
+    @When("website is open accept all cookie")
+    public void website_is_open_accept_all_cookie() {
+        WEB_DRIVER.findElement(By.id("onetrust-accept-btn-handler")).click();
     }
+
+    @Then("start search {string}")
+    public void start_search(String searchParam) {
+        WebElement searchButton =
+                WEB_DRIVER.findElement(By.xpath("/html/body/header/nav/ul[1]/li[2]/button"));
+        searchButton.click();
+        WebElement searchField = WEB_DRIVER.findElement(By.id("main-search"));
+        searchField.sendKeys(searchParam);
+        searchField.submit();
+    }
+
+    @Then("click on first image")
+    public void click_on_first_image() {
+        WEB_DRIVER.findElement(By.className("image-container")).click();
+    }
+
+    @Then("choose the size")
+    public void choose_the_size() {
+        WEB_DRIVER.findElement(By.className("button-buy")).click();
+        WebDriverWait waiter = new WebDriverWait(WEB_DRIVER, Duration.ofSeconds(30));
+
+        WEB_DRIVER.findElements(By.className("option")).get(3).click();
+    }
+
+    @Then("click add button")
+    public void click_add_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 }
--- target/classes/ru.sf/scenario.feature
+++ target/classes/ru.sf/scenario.feature
@@ -1,5 +1,8 @@
-Feature: Is it Friday yet?
-  Scenario: Sunday is not Friday
-    Given today is 'Friday'
-    When I ask about  whether it's Friday yet
-    Then I should be told 'Nope'
+Feature: Filling card
+  Scenario: Add item to card
+    Given url of H&M 'https://www2.hm.com/ru_ru/index.html'
+    When website is open accept all cookie
+    Then start search 'ремень'
+    Then click on first image
+    And choose the size
+    Then click add button
